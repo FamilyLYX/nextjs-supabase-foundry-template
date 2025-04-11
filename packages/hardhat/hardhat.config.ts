61da437e38bc66eb5938433178fd572e94e6543f
Bhavya Gor <bhavya.gor9999@gmail.com>
2025-04-11 13:33:29 +0530

    added basic stuff

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -1,8 +1,40 @@
-import type { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox-viem";
+import { HardhatUserConfig } from "hardhat/config";
+import { NetworkUserConfig } from "hardhat/types";
+import "@nomicfoundation/hardhat-toolbox";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+function getTestnetChainConfig(): NetworkUserConfig {
+  const config: NetworkUserConfig = {
+    url: "https://rpc.testnet.lukso.network",
+    chainId: 4201,
+  };
+
+  if (process.env.PRIVATE_KEY !== undefined) {
+    config["accounts"] = [process.env.PRIVATE_KEY];
+  }
+
+  return config;
+}
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  typechain: {
+    outDir: "typechain-types",
+    target: "ethers-v6",
+  },
+  networks: {
+    luksoTestnet: getTestnetChainConfig(),
+  },
 };
 
 export default config;
